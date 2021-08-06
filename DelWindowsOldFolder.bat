e925d137ce631d445a70fb26cb74be4dc06a353c
andras-varro <72469527+andras-varro@users.noreply.github.com>
2021-08-06 10:51:14 -0400

    Add files via upload

---
+++ DelWindowsOldFolder.bat
@@ -0,0 +1,11 @@
+@echo off
+IF "%1"=="" GOTO Usage
+takeown /R /A /F %1 /D Y
+icacls %1 /t /c /grant Everyone:(OI)(CI)F
+attrib -R -A -H -S %1\*.* /S /D
+del /f/s/q %1 > nul
+rmdir /s/q %1
+GOTO End
+:Usage
+echo Usage: DelWindowsOldFolder [FolderName]
+:End 
