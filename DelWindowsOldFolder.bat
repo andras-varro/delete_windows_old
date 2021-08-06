50e90c5de58440b144d30b9bd6101f744ff3ad9f
andras-varro <72469527+andras-varro@users.noreply.github.com>
2021-08-06 10:55:48 -0400

    Update DelWindowsOldFolder.bat

--- DelWindowsOldFolder.bat
+++ DelWindowsOldFolder.bat
@@ -1,7 +1,7 @@
 @echo off
 IF "%1"=="" GOTO Usage
 takeown /R /A /F %1 /D Y
-icacls %1 /t /c /grant Everyone:(OI)(CI)F
+icacls %1 /t /c /grant administrators:F
 attrib -R -A -H -S %1\*.* /S /D
 del /f/s/q %1 > nul
 rmdir /s/q %1
