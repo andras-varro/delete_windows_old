f158ce191225b4e570c07864af9f8d605b1498bb
andras-varro <72469527+andras-varro@users.noreply.github.com>
2021-08-06 13:11:11 -0400

    Update DelWindowsOldFolder.bat

--- DelWindowsOldFolder.bat
+++ DelWindowsOldFolder.bat
@@ -1,8 +1,15 @@
 @echo off
-IF "%1"=="" GOTO Usage
-takeown /R /A /F %1 /D Y
-icacls %1 /t /c /grant administrators:F
-attrib -R -A -H -S %1\*.* /S /D
+IF [%1]==[] GOTO Usage
+echo Delete whatever we can
+del /f/s/q %1 > nul 2>&1
+rmdir /s/q %1 > nul 2>&1
+echo Taking ownership of %1
+takeown /R /A /F %1 /D Y > nul
+echo Granting access to %1
+icacls %1 /t /c /q /l /grant administrators:F 
+echo Removing attributes
+attrib -R -A -H -S %1\*.* /S /D 
+echo Delete remaining items
 del /f/s/q %1 > nul
 rmdir /s/q %1
 GOTO End
